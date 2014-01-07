18119387d8f0fcdd01b151dbfe30c2d00397bf00
billy <billy1341@gmail.com>
2014-01-07 20:59:22 +0800

    完成乘法 待測試

--- DSPIC_final.srcs/sources_1/new/mult_32bits_float.v
+++ DSPIC_final.srcs/sources_1/new/mult_32bits_float.v
@@ -5,10 +5,13 @@ module mult_32bits_float(out,in1,in2);
 output `BITS_RANGE out;
 input `BITS_RANGE in1,in2;
 wire sign;
+wire [`EXPONENT_BITS-1:0]exponent_add_result;
+wire [`FRACTION_BITS+`FRACTION_BITS-1:0]fraction_mul_result;
 
-assign sign = in1[`SIGN_BITS_START]^in2[`SIGN_BITS_START];
+assign out = {sign, exponent_add_result, fraction_mul_result[`FRACTION_BITS+`FRACTION_BITS-1:`FRACTION_BITS]};
 
-
-
+assign sign = in1[`SIGN_BITS_START] ^ in2[`SIGN_BITS_START];
+assign exponent_add_result = in1`EXPONENT_BITS_RANGE - `EXPONENT_BITS_BIAS + in2`EXPONENT_BITS_RANGE;
+assign fraction_mul_result = in1`FRACTION_BITS_RANGE * in2`FRACTION_BITS_RANGE;
 
 endmodule
