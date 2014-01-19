052306da1356d6c521dec3122a0b7a44166762f2
Leo <u9761130@oz.nthu.edu.tw>
2014-01-19 22:38:32 +0800

    寫testbench

--- DSPIC_final.srcs/sim_1/new/test_fft_stage.v
+++ DSPIC_final.srcs/sim_1/new/test_fft_stage.v
@@ -1,26 +1,34 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2014/01/19 22:28:37
-// Design Name: 
-// Module Name: test_fft_stage
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+`include "params_sim.v"
+`include "../../sources_1/new/params.v"
 
+module test_fft_stage();
+reg `BITS_RANGE in1_real,in_imag;
+wire `BITS_RANGE out_real,out_imag;
+reg clk,rst,en;
 
-module test_fft_stage(
+initial
+begin
+clk = 1'b 0;
+$monitor("%0dns: out_r:%b"  , $stime, in_real);
+$monitor("%0dns: out_i:%b"  , $stime, in_imag);
 
-    );
+en = 1'b1;
+rst = 1'b0;
+#1
+rst = 1'b1;
+in_real = `NUM1;
+in_imag = `NUM0;
+#2
+in_real = `NUM1;
+in_imag = `NUM0;
+
+#2
+#2
+#2
+$finish();
+end
+
+always
+	#1 clk = ~clk;
 endmodule
